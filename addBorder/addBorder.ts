4e4bbd4530da4118d409428acd1067bbafdf7459
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-01-18 15:44:21 -0800

    completed addBorder with improvement

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,22 @@
 export function addBorder(picture: string[]): string[] {
+  // const borderWidth = picture[0].length + 2;
+  // let wall = '';
 
+  // for (let i = 0; i < borderWidth; i++) {
+  //   wall = wall.concat('*');
+  // }
+
+  // The above code simplifies to:
+  const wall = '*'.repeat(picture[0].length + 2);
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = '*'.concat(picture[i], '*');
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
