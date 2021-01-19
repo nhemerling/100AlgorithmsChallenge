ff317c0ddfe976b96e23062db1e8e6da4b838082
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-01-18 16:06:33 -0800

    completed adjacentElementsProduct

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,9 +1,12 @@
 export function addTwoDigits(n: any): number {
   const nums = n.toString().split('');
 
-  return nums.reduce((a: string, b: string) => {
-    return parseInt(a) + parseInt(b);
-  });
+  // return nums.reduce((a: string, b: string) => {
+  //   return parseInt(a) + parseInt(b);
+  // });
+
+  // The above can be simplified to:
+  return parseInt(nums[0]) + parseInt(nums[1]);
 }
 
 console.log(addTwoDigits(29));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,16 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  let greatestProduct = inputArray[0] * inputArray[1];
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    let product = inputArray[i] * inputArray[i + 1];
+    if (product > greatestProduct) {
+      greatestProduct = product;
+    }
+    // Also works instead of th if statement:
+    // greatestProduct = greatestProduct < product ? product : greatestProduct;
+  }
+
+  return greatestProduct;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
