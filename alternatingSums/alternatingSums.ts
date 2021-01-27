55435e247ad8dc3227fdf37848df4f2a39f3a0aa
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-01-27 10:49:57 -0800

    completed alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 export function alternatingSums(a: number[]): number[] {
+  const teamWeights = [0, 0];
+  a.forEach((weight, i) => {
+    if (i % 2 === 0) {
+      teamWeights[0] += weight;
+    } else {
+      teamWeights[1] += weight;
+    }
+  });
 
+  return teamWeights;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
