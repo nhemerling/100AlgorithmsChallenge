d237eab8d023d7a697ba9e439ef5eaa2e4b9bf7d
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-03-23 15:34:31 -0700

    complete arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,16 @@
 export function arrayChange(inputArray: number[]): number {
+  let count = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      const difference = inputArray[i] + 1 - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i] + 1;
+      count += difference;
+    }
+  }
+  console.log(inputArray);
+  return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 2, 1]));
